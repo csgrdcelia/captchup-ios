a5b47e2ad77973f7f5d883b991f2028d3fba2f6a
csgrdcelia <casagrande.celia@gmail.com>
2019-05-01 07:25:54 -0700

    create base structure of tab bar controller

---
+++ captchup/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  captchup
+//
+//  Created by Celia Casagrande on 01/05/2019.
+//  Copyright © 2019 iosesgi. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ captchup/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  captchup
+//
+//  Created by Celia Casagrande on 01/05/2019.
+//  Copyright © 2019 iosesgi. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
