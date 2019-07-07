0d65d215efd974da74c16d2cc135165b52af0d28
csgrdcelia <casagrande.celia@gmail.com>
2019-07-07 13:02:29 -0700

    Redirect to created level

--- captchup/NewImageViewController.swift
+++ captchup/NewImageViewController.swift
@@ -11,6 +11,7 @@ import Alamofire
 
 class NewImageViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
 
+    @IBOutlet weak var playButton: UIButton!
     @IBOutlet weak var pickImageButton: UIButton!
     @IBOutlet var imageView: UIImageView!
     var imagePicker = UIImagePickerController()
@@ -24,6 +25,19 @@ class NewImageViewController: UIViewController, UINavigationControllerDelegate, 
     
     @IBAction func pickImageButton_OnClick(_ sender: Any) {
         launchImagePicker()
+    }
+    
+    @IBAction func playButton_OnCick(_ sender: Any) {
+        self.performSegue(withIdentifier: "showLevel", sender: uploadLevel)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "showLevel" {
+            if let nextViewController = segue.destination as? GameViewController {
+                guard let level = sender as? Level else { return }
+                nextViewController.level = level
+            }
+        }
     }
     
     func launchImagePicker() {
@@ -64,20 +78,27 @@ class NewImageViewController: UIViewController, UINavigationControllerDelegate, 
             
             multipartFormData.append(imageData!, withName: "image", fileName: "image.jpg", mimeType: "image/jpeg")
             
-        }, usingThreshold: UInt64.init(), to: apiUrl, method: .post, headers: headers) { (result) in
+        }, usingThreshold: UInt64.init(), to: apiUrl, method: .post, headers: headers)
+        { (result) in
             switch result{
             case .success(let upload, _, _):
-                upload.responseJSON { response in
+                upload
+                    .validate(statusCode: 200..<300)
+                    .responseJSON { response in
                     do {
                         guard let data = response.data else { return }
                         self.uploadLevel = try JSONDecoder().decode(Level.self, from: data)
-                       self.displayLevelIsUpload()
+                        self.displayLevelIsUpload()
                     } catch {
                         return
                     }
                     
                 }
             case .failure(let error):
+                let alert = UIAlertController(title: "Alert", message: error.localizedDescription, preferredStyle: .alert)
+                let OKAction = UIAlertAction(title: "Alert " + ApiManager.token!, style: .default)
+                alert.addAction(OKAction)
+                self.present(alert, animated: true, completion: nil)
                 print("Error in upload: \(error.localizedDescription)")
                 
             }
@@ -86,6 +107,7 @@ class NewImageViewController: UIViewController, UINavigationControllerDelegate, 
     
     func displayLevelIsUpload() {
         pickImageButton.isHidden = true
+        playButton.isHidden = false
     }
     
     
