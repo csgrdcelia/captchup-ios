b8822a72f090f3857b4193df2ff7c2a46acb8b77
csgrdcelia <casagrande.celia@gmail.com>
2019-07-07 12:20:28 -0700

    Upload selected image from gallery to server for level creation

--- captchup/NewImageViewController.swift
+++ captchup/NewImageViewController.swift
@@ -7,11 +7,14 @@
 //
 
 import UIKit
+import Alamofire
 
 class NewImageViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
 
+    @IBOutlet weak var pickImageButton: UIButton!
     @IBOutlet var imageView: UIImageView!
     var imagePicker = UIImagePickerController()
+    var uploadLevel: Level?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +22,7 @@ class NewImageViewController: UIViewController, UINavigationControllerDelegate, 
         // Do any additional setup after loading the view.
     }
     
-    @IBAction func pickImageButton(_ sender: Any) {
+    @IBAction func pickImageButton_OnClick(_ sender: Any) {
         launchImagePicker()
     }
     
@@ -44,9 +47,48 @@ class NewImageViewController: UIViewController, UINavigationControllerDelegate, 
         if let image = info[.originalImage] as? UIImage {
             imageView?.contentMode = UIView.ContentMode.scaleAspectFill
             imageView?.image = image
+            createLevel(image: image)
             dismiss(animated: true, completion: nil)
         }
     }
+    
+    func createLevel(image: UIImage) {
+        let headers: HTTPHeaders = [
+            "Authorization": ApiManager.token!,
+            "Content-type": "multipart/form-data"
+        ]
+        let apiUrl: String = ApiManager.apiUrl + "level/create"
+        let imageData = image.jpegData(compressionQuality: 0.5)
+        
+        Alamofire.upload(multipartFormData: { (multipartFormData) in
+            
+            multipartFormData.append(imageData!, withName: "image", fileName: "image.jpg", mimeType: "image/jpeg")
+            
+        }, usingThreshold: UInt64.init(), to: apiUrl, method: .post, headers: headers) { (result) in
+            switch result{
+            case .success(let upload, _, _):
+                upload.responseJSON { response in
+                    do {
+                        guard let data = response.data else { return }
+                        self.uploadLevel = try JSONDecoder().decode(Level.self, from: data)
+                       self.displayLevelIsUpload()
+                    } catch {
+                        return
+                    }
+                    
+                }
+            case .failure(let error):
+                print("Error in upload: \(error.localizedDescription)")
+                
+            }
+        }
+    }
+    
+    func displayLevelIsUpload() {
+        pickImageButton.isHidden = true
+    }
+    
+    
     /*
     // MARK: - Navigation
 
